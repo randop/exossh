eedd509c2f8c497342eb5d69d1263d82ea713508
Randolph Ledesma <randopledesma+gitlab@hotmail.com>
2026-05-23 08:48:58 +0000

    build(script): Set script executable permissions

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -eu
 
-mkdir .build
+mkdir -p .build
 rm -f .build/exossh
 
-gcc -g -O0 -Wall -Wextra -fsanitize=address -fno-omit-frame-pointer -o .build/exossh src/main.c $(pkg-config --cflags --libs libuv)
+gcc -g -O0 -Wall -Wextra -o .build/exossh src/main.c $(pkg-config --cflags --libs libuv)
 
 chmod +x .build/exossh
